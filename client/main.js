f5c7de7d33856a818ef72f9ba1b929bbf45163f9
colette <colette.believe@gmail.com>
2022-08-08 07:33:31 -0600

    second commit

--- client/main.js
+++ client/main.js
@@ -1,5 +1,7 @@
 const complimentBtn = document.getElementById("complimentButton")
 
+
+
 const getCompliment = () => {
     axios.get("http://localhost:4000/api/compliment/")
         .then(res => {
@@ -15,9 +17,32 @@ const fortuneBtn = document.getElementById("fortuneButton")
 const getFortune = () => {
     axios.get("http://localhost:4000/api/fortune/")
     .then(res => {
-        const result = res.result;
+        const result = res.data;
         alert(result);
+    })       
+         .catch(err => {
+        console.log(err.response.data);
     })
-}
+};
+
+const createNote = (body) => {
+    axios.post("http://localhost:4000/api/", body)
+        .then((res) => {
+            display_sets(res.data)
+        })
+        .catch(err => {
+            console.log(err.response.data);
+        })
+};
+
+const delete_set = (id) => {
+    axios.delete(`"http://localhost:4000/api/"/${id}`)
+        .then((res) => {
+            display_sets(res.data)
+        })
+        .catch(err => {
+            console.log(err.response.data);
+        })
+    }
 
 fortuneBtn.addEventListener('click', getFortune)
--- server/index.js
+++ server/index.js
@@ -1,18 +1,38 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
-
 app.use(cors());
-
 app.use(express.json());
 
-const { getCompliment } = require('./controller')
+const positiveThoughts = [
+    { id: 1, set_name: "1PT"},
+    { id: 2, set_name: "2PT"},
+    { id: 3, set_name: "3PT"},
+]
+let baseID = 4
+
+const { getCompliment, getFortune } = require('./controller')
 
 app.get("/api/compliment", getCompliment);
-
-const { getFortune } = require('./controller');
-
 app.get("/api/fortune", getFortune);
+
+app.post("/api/positive", (request, response) => {
+    let new_set = {//added
+        id: base_id,
+        set_name: request.body.set_name
+    }
+    positiveThoughts.push(new_set)
+    base_id += 1
+    response.status(200).send(positiveThoughts)
+});
+
+app.delete("/api/positive/:set_id", (request, response) => {
+    console.log(request.params.set_id);
+    const set = positiveThoughts.find( (set_object) => set_object.id === parseInt(request.params.set_id))
+    positiveThoughts.splice(request.params.set_id -1, 1)
+    base_id--
+    response.status(200).send(positiveThoughts)
+});
+
 
 app.listen(4000, () => console.log("Server running on 4000"));
